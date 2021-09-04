5ef597b66d83b71a68fe5caf6778341f7c8d34e1
Ignacio Losiggio <iglosiggio@dc.uba.ar>
2021-09-04 18:06:22 -0300

    Agregar makefile que buildea en cada tamaño de float

--- src/config.h
+++ src/config.h
@@ -1,9 +1,18 @@
 #pragma once
 
 // Si quieren forzar algún tipo especifico pueden pasar
-// -DMETNUM_FLOAT_T=<tipo> cómo parámetro al compilador
-#ifdef METNUM_FLOAT_T
-using metnum_float_t = METNUM_FLOAT_T;
+// -DMETNUM_FLOAT_WIDTH=<cantidad_de_bits> cómo parámetro al compilador
+#ifdef METNUM_FLOAT_WIDTH
+#  if METNUM_FLOAT_WIDTH == 32
+using metnum_float_t = float;
+#  elif METNUM_FLOAT_WIDTH == 64
+using metnum_float_t = double;
+#  elif METNUM_FLOAT_WIDTH == 80
+using metnum_float_t = long double;
+#  else
+#    error "No conozco un float con el tamaño solicitado"
+#  endif
+#include <climits>
 #else
 // Sino comenten y descomenten estas líneas!
 //using metnum_float_t = float;
